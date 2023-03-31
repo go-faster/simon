8eb6e7e2e06ee5ddad6a57aa731e432853aabf2f
Aleksandr Razumov <ernado@ya.ru>
2023-03-31 13:42:08 +0300

    chore: lint

--- internal/server/server.go
+++ internal/server/server.go
@@ -12,14 +12,11 @@ type Server struct{}
 
 var _ oas.Handler = (*Server)(nil)
 
-func (s Server) Status(ctx context.Context) (*oas.Status, error) {
-	return &oas.Status{
-		Message: "ok",
-	}, nil
-
+func (s Server) Status(_ context.Context) (*oas.Status, error) {
+	return &oas.Status{Message: "ok"}, nil
 }
 
-func (s Server) NewError(ctx context.Context, err error) *oas.ErrorStatusCode {
+func (s Server) NewError(_ context.Context, err error) *oas.ErrorStatusCode {
 	return &oas.ErrorStatusCode{
 		StatusCode: 500,
 		Response: oas.Error{
